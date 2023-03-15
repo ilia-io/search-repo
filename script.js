0cfdf44c5186e17d9b9d9d44fde3b3ca746e6e72
ilia-io <likeapu@gmail.com>
2023-03-15 19:36:19 +0300

    feat: basic project structure

---
+++ script.js
@@ -0,0 +1,23 @@
+const API_URL = 'https://api.github.com/search/repositories?q=tetris';
+
+async function getRepos() {
+  const response = await fetch(API_URL);
+  const result = await response.json();
+  console.log(result.items);
+  return result;
+}
+
+getRepos();
+
+function makeRepoItem(){
+const data = getRepos()
+
+const name = data.name
+const url = data.url;
+
+const description = data.description;
+const language = data.language;
+const owner = data.owner.login
+
+
+}
